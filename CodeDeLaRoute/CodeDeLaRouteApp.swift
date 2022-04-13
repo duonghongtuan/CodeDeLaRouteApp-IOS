301ba3309be1460cb28e51f5526082d4f69969c3
duonghongtuan <hongtuan2805@gmail.com>
2022-04-13 16:09:32 +0700

    Initial Commit

---
+++ CodeDeLaRoute/CodeDeLaRouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeDeLaRouteApp.swift
+//  CodeDeLaRoute
+//
+//  Created by Nhung Nguyen on 13/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct CodeDeLaRouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeDeLaRoute/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CodeDeLaRoute
+//
+//  Created by Nhung Nguyen on 13/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
